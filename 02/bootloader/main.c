b1a383a2ea4811b6347010d3d89f5fd7091de248
Yuichi Tateno <hotchpotch@gmail.com>
2016-05-13 12:41:26 +0900

    up

--- 02/bootloader/main.c
+++ 02/bootloader/main.c
@@ -6,6 +6,15 @@ int main(void)
 {
   serial_init(SERIAL_DEFAULT_DEVICE);
   puts("Hello World!\n");
+
+  putxval(0x10, 0);
+  puts("\n");
+  putxval(0x10, 2);
+  puts("\n");
+  putxval(0xffff, 2);
+  puts("\n");
+  putxval(0xffff, 6);
+  puts("\n");
   while (1)
     ;
 
