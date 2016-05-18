a7b88ed4c245013eda9b8d6427c2773eccca3ed6
Yuichi Tateno <hotchpotch@gmail.com>
2016-05-18 17:06:17 +0900

    Fix syntax

--- 07/bootloader/interrupt.h
+++ 07/bootloader/interrupt.h
@@ -9,7 +9,7 @@ typedef short softvec_type_t;
 
 typedef void (*softvec_handler_t)(softvec_type_t type, unsigned long sp);
 
-#define SOFTVECS ((softvec_handler_t *)(SOFTVEC_ADDR))
+#define SOFTVECS ((softvec_handler_t *)SOFTVEC_ADDR)
 
 #define INTR_ENABLE asm volatile ("andc.b #0x3f,ccr")
 #define INTR_DISABLE asm volatile ("orc.b #0xc0,ccr")
