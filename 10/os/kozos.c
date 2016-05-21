4486557556828783f5a9b0bf397cf9a710f6bf9c
Yuichi Tateno <hotchpotch@gmail.com>
2016-05-21 18:54:57 +0900

    Impl memory

--- 10/os/kozos.c
+++ 10/os/kozos.c
@@ -4,6 +4,7 @@
 #include "intr.h"
 #include "interrupt.h"
 #include "syscall.h"
+#include "memory.h"
 #include "lib.h"
 
 #define THREAD_NUM 6
@@ -213,6 +214,18 @@ static int thread_chpri(int priority)
 }
 
 
+static void *thread_kmalloc(int size)
+{
+  putcurrent();
+  return kzmem_alloc(size);
+}
+
+static int thread_kmfree(char *p)
+{
+  kzmem_free(p);
+  putcurrent();
+  return 0;
+}
 
 
 static int setintr(softvec_type_t type, kz_handler_t handler)
@@ -250,6 +263,12 @@ static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
       break;
     case  KZ_SYSCALL_TYPE_CHPRI:
       p->un.chpri.ret = thread_chpri(p->un.chpri.priority);
+      break;
+    case  KZ_SYSCALL_TYPE_KMALLOC:
+      p->un.kmalloc.ret = thread_kmalloc(p->un.kmalloc.size);
+      break;
+    case  KZ_SYSCALL_TYPE_KMFREE:
+      p->un.kmfree.ret = thread_kmfree(p->un.kmfree.p);
       break;
     default:
       break; 
@@ -304,6 +323,8 @@ static void thread_intr(softvec_type_t type, unsigned long sp)
 void kz_start(kz_func_t func, char *name, int priority, int stacksize,
     int argc, char *argv[])
 {
+  kzmem_init();
+
   current = NULL;
 
   memset(readyque, 0, sizeof(readyque));
@@ -331,6 +352,4 @@ void kz_syscall(kz_syscall_type_t type, kz_syscall_param_t *param)
   current->syscall.param = param;
   asm volatile ("trapa #0");
 }
-
-int test08_1_main(int argc, char *argv[]);
 
