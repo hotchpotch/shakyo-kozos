e034cbb23a6d6515036df3d7a74f083fb679baf2
Yuichi Tateno <hotchpotch@gmail.com>
2016-05-23 09:25:20 +0900

    Syakyo

--- 12/os/main.c
+++ 12/os/main.c
@@ -16,9 +16,11 @@ static int start_threads(int argc, char *argv[])
   test09_2_id = kz_run(test09_2_main, "th_test09_2", 2, 0x100, 0, NULL);
   test09_3_id = kz_run(test09_3_main, "th_test09_3", 3, 0x100, 0, NULL);
   kz_run(test10_1_main, "th_test10_1", 1, 0x100, 0, NULL);
-  */
   kz_run(test11_1_main, "th_test11_1", 1, 0x100, 0, NULL);
   kz_run(test11_2_main, "th_test11_2", 2, 0x100, 0, NULL);
+  */
+  kz_run(consdrv_main, "th_consdrv_main", 1, 0x100, 0, NULL);
+  kz_run(command_main, "th_command_main", 8, 0x100, 0, NULL);
 
   kz_chpri(15);
   INTR_ENABLE;
